613465b2a09c539b2b5c269dc127502c62f1e7d2
Hernán Ezequiel Novara <her.novara@hotmail.com>
2023-12-03 20:45:58 -0300

    Ejercicio 5

--- js/main.js
+++ js/main.js
@@ -5,77 +5,78 @@ document.addEventListener("DOMContentLoaded", function() {
 // Ejercicio 3:
 const msg = document.getElementById('origen').value = `<p>Este contenido <strong> está listo</strong><br>para ser editado y pasarlo abajo. </p>`;
 
-
 // Ejercicio 4:
 let e = document.getElementsByTagName("input");
-let bo = document.getElementsByTagName("button");
+let bo = document.getElementsByTagName("button")[0];
 
-let eNum = "";
-function habilitar() {
-    for (let index = 0; index < e.length; index++) {
-        e[index].disabled = false;
-    }
-    return eNum;
-}
-habilitar();
+let click = document.getElementById('origen');
 
-function habilitar2() {
-    for (let index = 0; index < bo.length; index++) {
-        bo[index].disabled = false;
+click.addEventListener ("click", function(a){
+    habilitar(e,bo);
+})
+
+
+function habilitar(e, bo) {
+    for (let i = 0; i < e.length; i++) {
+        e[i].disabled =false
     }
-    return eNum;
+    bo.disabled =false
 }
-habilitar2();
 
 // Ejercicio 5:
 let one = document.getElementById("btn-reemplazar");
 one.addEventListener("click", reemplazar); 
 function reemplazar() {
     document.getElementById('destino').innerHTML =`${msg}`;
+    document.getElementById("origen").value = "";
 }
 
+let arrayAgregar = document.getElementsByClassName("btn-agregar");
 
-let allClass = document.getElementsByClassName("btn-agregar");
-let classNum = "";
 function forClass() {
-    for (let i = 0; i < allClass.length; i++) {
-        switch (i) {
-            case 0:
-                // console.log("1")
-                addEventListener("click", Agregar); 
-                function Agregar() {
-                    document.getElementById('destino').innerHTML =`${msg}`;
-                }
-                break;
-            case 1:
-                // console.log("2")
-                addEventListener("click", Agregar5veces); 
-                function Agregar5veces() {
-                    document.getElementById('destino').innerHTML =`${msg.repeat(5)}`;
-                }
-                break; 
-            case 2:
-                //  console.log("3")
-                addEventListener("click", Agregar10veces); 
-                function Agregar10veces() {
-                   document.getElementById('destino').innerHTML =`${msg.repeat(10)}`;
-                }
-                break; 
+    for (let i = 0; i < arrayAgregar.length; i++) {
+        arrayAgregar[i].addEventListener("click", function(event) {
+            switch (i) {
+                case 0:
+                    document.getElementById('destino').innerHTML = `${msg.repeat(1)}`;
+                    break;
+                case 1:
+                    document.getElementById('destino').innerHTML = `${msg.repeat(5)}`;
+                    break;
+                case 2:
+                    document.getElementById('destino').innerHTML = `${msg.repeat(10)}`;
+                    break;
                 case 3:
-                //  console.log("4")
-                addEventListener("click", AgregarNveces); 
-                function AgregarNveces() {
-                   n = prompt("Cuantas veces agregar?");
-                   document.getElementById('destino').innerHTML =`${msg.repeat(n)}`;
-                }
-                break; 
-            default:
-                break;
-        }
+                    let n = prompt("¿Cuántas veces quieres agregar el texto?");
+                    if (n !== null) {
+                        document.getElementById('destino').innerHTML = `${msg.repeat(parseInt(n))}`;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        });
     }
-    return classNum;
 }
 forClass();
+
+// botonVaciar = document.querySelector(".btn-vaciar");
+
+// botonVaciar.addEventListener("click", function()) {
+//     let contenidoDiv = document.getElementById("destino");
+//     contenidoDiv.innerHTML = "";
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
